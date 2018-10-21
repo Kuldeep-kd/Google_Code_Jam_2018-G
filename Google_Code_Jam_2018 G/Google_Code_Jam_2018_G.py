88350c2e0bb33156e484f02fa9e924f6bf6e4355
Kuldeep Thakre <kuldeep-kd@live.com>
2018-10-21 05:49:47 +0530

    Added Code

--- Google_Code_Jam_2018 G/Google_Code_Jam_2018_G.py
+++ Google_Code_Jam_2018 G/Google_Code_Jam_2018_G.py
@@ -1,1 +1,32 @@
+#Kuldeep Thakre kuldeep-kd@live.com
+import math
 
+def calc(data_list):
+    if data_list[0] == 1:
+        return (data_list[0] % data_list[2])
+    return (data_list[0]**math.factorial(data_list[1])) % data_list[2]
+
+def get_list(filename="input_file.txt"):
+    ret_list = []
+    file = open(filename,"r")
+    line = file.readline()
+#Generate list of elements as list of list of strs
+    for _ in range(int(line)+1):
+        if '\n' in line:
+            ret_list.append((list( line.strip('\n').split(' ') )))
+        else:
+            ret_list.append((list( line.split(' ') )))
+        line = file.readline()
+    file.close()
+    ret_list.pop(0) #Remove very First element i.e No. Test Cases
+    # Using ListComprehension Converted list of string list tolist of int list
+    print("Done")
+    return [[int(i), int(j), int(k)] for [i,j,k] in ret_list] 
+lst = get_list()
+
+
+output = open("output_file.txt","w+")
+for case in range(len( lst )):
+    output.write( f"Case #{case+1}: {calc( lst.pop(0) )}\n") 
+    print(f"Case {case+1} done\n")
+output.close()
